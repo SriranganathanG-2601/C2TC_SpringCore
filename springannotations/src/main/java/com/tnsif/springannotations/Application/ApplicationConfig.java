45fe89b440e846bab41364999f6ced440543a69c
Ranga <ranga@Ranga>
2025-11-06 19:02:58 +0530

    Spring Annotations completed

---
+++ springannotations/src/main/java/com/tnsif/springannotations/Application/ApplicationConfig.java
@@ -0,0 +1,10 @@
+//Program to demonstrate configuration using annotations
+
+
+package com.tnsif.springannotations.Application;
+
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan({"com.springannotations"})
+public class ApplicationConfig {
+
+}
---
+++ springannotations/src/test/java/com/tnsif/springannotations/SpringannotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.springannotations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringannotationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
